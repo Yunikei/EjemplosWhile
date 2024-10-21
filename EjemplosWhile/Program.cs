36b5129ab8b9764c0e67a47af7bf47f7d18da11d
Yunkei <alva_alcantar24@unikino.edu.mx>
2024-10-21 07:40:16 -0600

    Agregar archivos de proyecto.

---
+++ EjemplosWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EjemplosWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int repetir = 100;
+            int i = 0; //este es un contador
+
+            while (i < repetir)
+            {
+                i++;
+                Console.WriteLine("Bienvenido  i =" + i);
+            }
+            
+            
+            
+        }
+    }
+}
